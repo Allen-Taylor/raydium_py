a44819a3e2b7fefb690493c9ff6a2f4a8a502dfd
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-08-31 19:14:13 +0200

    Delete raydium_py directory

--- raydium_py/config.py
+++
@@ -1,8 +0,0 @@
-from solana.rpc.api import Client
-from solders.keypair import Keypair #type: ignore
-
-PUB_KEY = ""
-PRIV_KEY = ""
-RPC = ""
-client = Client(RPC)
-payer_keypair = Keypair.from_base58_string(PRIV_KEY)
