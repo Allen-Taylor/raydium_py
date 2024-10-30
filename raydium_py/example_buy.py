c6c4a537d876a0602839a0ce95af24284391eea6
AL THE BOT FATHER <98119917+Allen-Taylor@users.noreply.github.com>
2024-10-30 04:02:59 +0100

    Delete raydium_py directory

--- raydium_py/example_buy.py
+++
@@ -1,9 +0,0 @@
-from raydium import buy
-from utils import get_pair_address
-
-# Buy Example
-token_address = "7GCihgDB8fe6KNjn2MYtkzZcRjQy3t9GHdC8uHYmW2hr" # POPCAT
-pair_address = get_pair_address(token_address)
-sol_in = .1
-slippage = 10
-buy(pair_address, sol_in, slippage)
